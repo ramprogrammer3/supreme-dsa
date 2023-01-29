481f09f34263a23719d6c9c49a13b8c5c000e337
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-01-29 17:14:35 +0530

    conditional-one

---
+++ basic/conditional.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score;
+    cout << "enter the score " << endl;
+    cin >> score;
+
+    if (score < 300)
+    {
+        cout << " India wins " << endl;
+    }
+    cout << "pakistan win " << endl;
+    return 0;
+}
