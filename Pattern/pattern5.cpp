7bd14b187f592938988ef8e032e1ab0c1c8d34f1
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-01-29 20:33:14 +0530

    inverted-half-pyramid

---
+++ Pattern/pattern5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "inverted half pyramid " << endl;
+
+    int n;
+    cin >> n;
+
+    for (int row = 0; row < n; row = row + 1)
+    {
+        for (int col = 0; col < n - row; col = col + 1)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
