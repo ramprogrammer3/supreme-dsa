324cd59a569f24f3b8570fe17338e070181d4620
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-01-29 19:07:49 +0530

    test

---
+++ basic/test1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    if(cin>>n){
+        cout << "hello ram " <<endl;
+    }
+
+    if(cout<< "ram"){
+        cout << " my name is ram " << endl;
+    }
+
+    return 0;
+}
