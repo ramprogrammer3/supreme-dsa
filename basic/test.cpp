51d28932d8e63bed534f66cc47d93483f8addce4
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-01-29 18:28:40 +0530

    test

---
+++ basic/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int bro;
+    cout << "Enter the number of bro : " << endl;
+    cin >> bro;
+
+    if(bro== 0){
+        cout << "Baat ban jayegi " << endl;
+    }else{
+        cout << "Baat nahi banegi " << endl;
+    }
+
+    return 0;
+}
