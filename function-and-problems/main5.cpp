9d6c1ce407f64125d8cfd1bccd39b6820b9f588b
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-08 17:32:52 +0530

    student-grade

---
+++ function-and-problems/main5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+char getGrade(int marks)
+{
+
+    if (marks >= 90)
+    {
+        return 'A';
+    }
+    else if (marks >= 80)
+    {
+        return 'B';
+    }
+    else if (marks >= 70)
+    {
+        return 'C';
+    }
+    else if (marks >= 60)
+    {
+        return 'D';
+    }
+    else
+    {
+        return 'E';
+    }
+}
+
+int main()
+{
+
+    int marks;
+    cout << "Enter marks : " << endl;
+
+    cin >> marks;
+
+    char grade = getGrade(marks);
+
+    cout << "Your grade is " << grade << endl;
+
+    return 0;
+}
