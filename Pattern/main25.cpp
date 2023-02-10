9ea9fc5433e913c8779ab29d9e4c567e706f43bc
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-10 17:10:18 +0530

    numeric-hollow-inverted-half-pyramid

---
+++ Pattern/main25.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n - row; col++)
+        {
+            if (row == 0)
+            {
+                cout << col + 1 << " ";
+            }
+            else if (row == n - 1)
+            {
+                cout << n;
+            }
+            else
+            {
+                if (col == 0)
+                {
+                    cout << row + 1 << " ";
+                }
+                else if (col == n - row - 1)
+                {
+                    cout << n;
+                }
+                else
+                {
+                    cout << "  ";
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
