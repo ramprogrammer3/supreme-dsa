a9fe3b462d543faaf3bfa116f2a0d614f1c7d697
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-11 20:43:52 +0530

    unique-element-in-array

---
+++ arrays/main9.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findUnique(vector<int> arr)
+{
+    int ans = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans = ans ^ arr[i];
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter the element : " << endl;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+
+    int uniqueElement = findUnique(arr);
+    cout << "Unique element is " << uniqueElement << endl;
+
+    return 0;
+}
