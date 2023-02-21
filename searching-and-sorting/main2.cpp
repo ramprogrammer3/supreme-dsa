72b0e5b799dd5307b483830bd415711233e3d956
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-21 07:56:47 +0530

    binary-search-algorithm

--- searching-and-sorting/main2.cpp
+++ searching-and-sorting/main2.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
+    vector<int> v{2, 4, 6, 8, 10, 12, 16};
+    bool ans = binary_search(v.begin(), v.end(), 20);
 
+    if(ans){
+        cout << "Element is found " << endl;
+    }else{
+        cout << "Element is not found " << endl;
+    }
     return 0;
 }
