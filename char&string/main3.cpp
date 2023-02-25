ad711f5b85dcfe899093219ba51119b4d81fe7d3
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-26 04:23:30 +0530

    replace-space-with-@

---
+++ char&string/main3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void replaceSpaces(char sentence[]){
+    // int i = 0;
+    int n = strlen(sentence);
+
+    for(int i = 0; i<n; i++){
+        if(sentence[i] == ' '){
+            sentence[i] = '@';
+        }
+    }
+
+
+}
+
+int main(){
+
+    char sentence[100];
+    cin.getline(sentence,100);
+
+    cout << sentence << endl;
+
+    replaceSpaces(sentence);
+
+    cout << sentence << endl;
+
+    return 0;
+}
