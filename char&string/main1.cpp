788865afae547be5f2604eff03aaa73aee6c9b4c
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-26 03:59:27 +0530

    lengthOfCharArray

---
+++ char&string/main1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int getLength(char name[])
+{
+    int length = 0;
+    int i = 0;
+
+    while (name[i] != '\0')
+    {
+        length++;
+        i++;
+    }
+    return length;
+}
+
+int main()
+{
+
+    char name[100];
+    cin.getline(name, 100);
+    cout << "length is " << getLength(name) << endl;
+    cout << "Lenght is " << strlen(name) << endl;
+    return 0;
+}
