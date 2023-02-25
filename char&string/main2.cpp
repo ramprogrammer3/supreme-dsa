be0acdd409ef273f0c05cde5ea803c4674f560bf
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-26 04:10:32 +0530

    reverse-char-Array

---
+++ char&string/main2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int getLength(char name[])
+{
+    int length = 0;
+    int i = 0;
+
+    while (name[i] != '\0')
+    {
+        length++;
+        i++;
+    }
+    return length;
+}
+
+void reverseCharArray(char name[])
+{
+    int i = 0;
+    int n = getLength(name);
+    int j = n - 1;
+
+    while (i <= j)
+    {
+        swap(name[i], name[j]);
+        i++;
+        j--;
+    }
+}
+
+int main()
+{
+
+    char name[100];
+    cin >> name;
+    cout << "Initially " << name << endl;
+    reverseCharArray(name);
+
+    cout << "After reverse " << name << endl;
+
+    return 0;
+}
