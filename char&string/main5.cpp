e0aa3822251e59de1f37f5be642522fc9af060a0
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-02-26 04:49:34 +0530

    convertIntoUppercase

---
+++ char&string/main5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void convertIntoUpperCase(char arr[])
+{
+    int n = strlen(arr);
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = arr[i] - 'a' + 'A';
+    }
+}
+
+int main()
+{
+
+    char arr[100] = "babbar";
+    cout << arr << endl;
+
+    convertIntoUpperCase(arr);
+    cout << arr << endl;
+
+    return 0;
+}
