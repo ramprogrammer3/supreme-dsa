85b761f72237de33653cd96077369bea6f5200a4
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-03-10 07:50:18 +0530

    pointer

---
+++ pointers/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+
+    int a = 5;
+    int *ptr = &a;
+    int *dursaPtr = ptr;
+
+    
+
+    // int *ptr = nullptr;
+    // cout << ptr << endl;
+    // cout << *ptr << endl;
+
+    // cout << "Hello world " << endl;
+
+
+    // int a = 5;
+    // int *p = &a; 
+    // cout << sizeof(p) << endl;
+
+    // char ch = 5;
+    // char *c = &ch;
+    // cout << sizeof(c) << endl;
+
+    // double d = 1.03;
+    // double *dtr = &d;
+    // cout << sizeof(dtr) << endl;
+
+    // int a = 5;
+    // //creating a pointer
+    // int *ptr = &a;
+
+    // // access the value ptr is pointing to
+    // cout << ptr << endl;
+    // cout << &a << endl;
+
+    // cout << *ptr << endl;
+    // cout << a << endl;
+
+    // int a = 5;
+    // cout << a << endl;
+    // cout << &a << endl;
+
+    // int b = 5;
+    // cout << &b << endl;
+
+
+    return 0;
+}
