4126d701db8e287472193034cd61dae3d9e3bcb1
ram kumar <rammaniyari@gmail.com>
2023-03-24 00:52:38 +0530

    max-lenght-of-rod

---
+++ Recursion-1/main12.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+int solve(int n, int x, int y, int z){
+
+    if(n== 0) return 0;
+
+    if(n<0) return INT_MIN;
+
+    int ans1 = solve(n-x,x,y,z)+1;
+    int ans2 = solve(n-y,x,y,z)+1;
+    int ans3 = solve(n-z,x,y,z)+1;
+
+    // int a = 0;
+    // if( n - x >= 0){
+    //     a = solve(n-x,x,y,z) +1;
+    // }
+
+    // int b = 0;
+    // if(n-y>=0){
+    //     b = solve(n-y,x,y,z) + 1;
+    // }
+
+    // int c = 0;
+    // if(n-z>=0){
+    //     c = solve(n-z,x,y,z) + 1;
+    // }
+
+    int ans = max(ans1,max(ans2,ans3));
+
+    return ans;
+
+}
+
+int main(){
+    int n = 7;
+    int x = 5;
+    int y = 2;
+    int z = 2;
+
+    // maximun length of rod
+    int ans = solve(n, x,y,z);
+
+    if(ans <0)
+    ans = 0;
+    cout << "Answer is : " << ans << endl;
+    
+
+    return 0;
+}
